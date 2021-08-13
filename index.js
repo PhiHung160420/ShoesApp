9c889ef511b8c70a2757c0f250968f7feb776485
PhiHung160420 <67174257+PhiHung160420@users.noreply.github.com>
2021-08-13 15:16:17 +0700

    login with facebook

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-if ((Platform.OS = 'ios')) {
+if (Platform.OS === 'ios') {
   Settings.initializeSDK();
 }
 
